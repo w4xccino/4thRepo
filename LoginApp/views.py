c661861603d08d49aa5b6941483aa9069f5b5d2d
epcbox <epcbox@protonmail.com>
2021-09-30 17:52:39 -0500

    fixed links

--- LoginApp/views.py
+++ LoginApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here.d
+def loginpage(request):
+     return render(request, 'login.html')
