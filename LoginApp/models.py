f0033a982b7cd07421d74abb112f5433de063e23
romario <illeniumr@gmail.com>
2021-10-01 18:51:57 -0600

    he modificado models.py agregando una clase

--- LoginApp/models.py
+++ LoginApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Models):
+	nombre=models.CharField(max_lenght=15)
+	apellido=models.CharField(max_lenght=15)
+	telefono=models.IntegerField()
+	correo=models.EmailField()
+	direccion=models.CharField(max_lenght=30)
+	sexo=models.CharField(max_lenght=1)
+	c.postal=models.IntegerField(max_lenght=5)
