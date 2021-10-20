71c9ca7fed0c5b7be8be046aeb5f300f6e82f215
w4xccino <dtoledoa@ulasalle.edu.pe>
2021-10-19 21:14:55 -0500

    consola admin arreglada, falta login

--- LoginApp/admin.py
+++ LoginApp/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Usuario
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
 
-admin.site.register(Usuario)
+class AdminAccount(UserAdmin):
+    list_display = ('username','email','date_joined', 'first_name','last_name','phone','direccion','c_postal','is_superuser')
+    search_fields = ('username','email', 'first_name')
+    readonly_fields = ('date_joined','last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+admin.site.register(Usuario,AdminAccount)
