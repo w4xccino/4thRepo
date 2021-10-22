5ed910bdbdd7f9396317e3a6d6739e45f43c4e44
2R0N4LD02 <romaritoxd@hotmail.com>
2021-10-22 14:55:56 -0500

    Index casi terminado y modificación de la base.html

--- WebPayments/urls.py
+++ WebPayments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import base
+from .views import base, index
 from django.urls import path
 urlpatterns = [
-    path("base/", base, name="base")
+    path("base/", base, name="base"), 
+    path("index/", index, name="index")
 ]
