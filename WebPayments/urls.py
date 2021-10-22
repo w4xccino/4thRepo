5cc511a5d4ebe8b1926262eb9ec277f2f3e0ddfe
2R0N4LD02 <romaritoxd@hotmail.com>
2021-10-22 18:23:05 -0500

    Modificaciones del login

--- WebPayments/urls.py
+++ WebPayments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import base, index
+from .views import base, index, proveedores
 from django.urls import path
 urlpatterns = [
     path("base/", base, name="base"), 
-    path("index/", index, name="index")
+    path("index/", index, name="index"),
+    path("proveedores/", proveedores, name="proveedores")
 ]
--- WebPayments/views.py
+++ WebPayments/views.py
@@ -3,3 +3,5 @@ def base(request):
     return render(request, 'WebPayments/base.html')
 def index(request):
     return render(request, 'WebPayments/index.html')
+def proveedores(request):
+    return render(request, 'WebPayments/proveedores.html')
