b01d47708d67843e68b74f850805ab48b146bbb8
romario <illeniumr@gmail.com>
2021-10-27 17:08:38 -0600

    he agregado el archivo listarproveedores

--- WebPayments/urls.py
+++ WebPayments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import base, index, proveedores
+from .views import base, index, proveedores, listarproveedores
 from django.urls import path
 urlpatterns = [
     path("index/", index, name="index"),
-    path("proveedores/", proveedores, name="proveedores")
+    path("proveedores/", proveedores, name="proveedores"),
+    path("listarproveedores/", listarproveedores, name="listarproveedores")
 ]
